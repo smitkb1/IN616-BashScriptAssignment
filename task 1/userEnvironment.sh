1b7c9ec576d9c435f8884d72081244497594e3e7
smitkb1 <smitkb1@student.op.ac.nz>
2021-06-05 23:19:14 +1200

    Added user entry number to task 1 script and create shared folder and group

--- task 1/userEnvironment.sh
+++ task 1/userEnvironment.sh
@@ -2,6 +2,7 @@
 
 file=$1
 
+#This checks if a CSV file was input as a command line argument, if not it will ask the user to pick one"
 if [[ ! -f $1 || -z $1 ]]
 then
 echo "Which CSV file would you like to read?"
@@ -10,6 +11,10 @@ fi
 
 if [[ -f $file ]]
 then
+
+entryCount=`cat $file | wc -l`
+((entryCount--))
+echo "$entryCount users will be added"
 
 IFS=";"
 i=0
@@ -22,16 +27,25 @@ firstname=`echo "$col1" | grep -oP '^[a-z]' `
 lastname=`echo "$col1" | grep -oP '(?<=\.)(.*?)(?=\@)'`
 username=${firstname}${lastname}
 password=`date -d "$col2" +'%m%Y'`
+group=`echo "$col3" | grep -oP '^\w+' `
 sharedFolder=$col4
 
-if [[! -d "$sharedFolder" ]]
+if [[ ! -d "$sharedFolder" ]]
 then
-sudo mkdir
+sudo mkdir /home/IN616-BashScriptAssignment/"task 1"/"$sharedFolder"
+fi
+sudo groupadd "$group"
 
+if [[ $? == 0 ]]
+then
+sudo chgrp -R "$group" /home/IN616-BashScriptAssignment/"task 1"/"$sharedFolder"
+sudo chmod -R 2775 /home/IN616-BashScriptAssignment/"task 1"/"$sharedFolder"
+fi
 
+sudo useradd -d /home/"$username" -m -s /bin/bash "$username"
 echo "$username"
 echo "$password"
-echo "$col3"
+echo "$group"
 echo "$col4"
 fi
 ((i++))
